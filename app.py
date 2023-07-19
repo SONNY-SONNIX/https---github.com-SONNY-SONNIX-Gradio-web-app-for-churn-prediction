bd179efae03260dcd58fe61ba06ba79a88d10dce
Sonny A-Otchie <sonny.otchie@yahoo.com>
2023-07-19 22:00:35 +0000

    app.py Update

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ def predict_churn(SeniorCitizen, Partner, Dependents, tenure, InternetService,
         "PaymentMethod", "MonthlyCharges", "TotalCharges"
     ])
 
-    pred = classifier.predict(input_df)
+    pred = model.predict(input_df)
     output = classify(pred[0])
 
     if output == "Customer will not Churn":
@@ -61,35 +61,50 @@ def predict_churn(SeniorCitizen, Partner, Dependents, tenure, InternetService,
     else:
         return [(1, output)]
 
+
+with block:
+    gr.Markdown(""" # Welcome to My Customer Churn Prediction App""")
+    input=[gr.inputs.Slider(minimum=0, maximum= 1, step=1, label="SeniorCitizen: Select 1 for Yes and 0 for No"),
+        gr.inputs.Radio(["Yes", "No"], label="Partner: Do You Have a Partner?"),
+        gr.inputs.Radio(["Yes", "No"], label="Dependents: Do You Have a Dependent?"),
+        gr.inputs.Number(label="tenure: How Long Have You Been with Vodafone in Months?"),
+        gr.inputs.Radio(["DSL", "Fiber optic", "No"], label="InternetService"),
+......]
+
 output = gr.outputs.HighlightedText(color_map={
     "Customer will not Churn": "green",
     "Customer will churn": "red"
 })
 
+predict_btn= gr.Button("Predict")
+     
+predict_btn.click(fn= predict_churn, inputs= input, outputs=output)
 
-iface = gr.Interface(title= "Customer Churn Prediction For Vodafone PLC",
-    fn=predict_churn,
-    inputs=[
-        gr.inputs.Slider(minimum=0, maximum= 1, step=1, label="SeniorCitizen: Select 1 for Yes and 0 for No"),
-        gr.inputs.Dropdown(["Yes", "No"], label="Partner: Do You Have a Partner?"),
-        gr.inputs.Dropdown(["Yes", "No"], label="Dependents: Do You Have a Dependent?"),
-        gr.inputs.Number(label="tenure: How Long Have You Been with Vodafone in Months?"),
-        gr.inputs.Dropdown(["DSL", "Fiber optic", "No"], label="InternetService"),
-        gr.inputs.Dropdown(["Yes", "No", "No internet service"], label="OnlineSecurity"),
-        gr.inputs.Dropdown(["Yes", "No", "No internet service"], label="OnlineBackup"),
-        gr.inputs.Dropdown(["Yes", "No", "No internet service"], label="DeviceProtection"),
-        gr.inputs.Dropdown(["Yes", "No", "No internet service"], label="TechSupport"),
-        gr.inputs.Dropdown(["Yes", "No", "No internet service"], label="StreamingTV"),
-        gr.inputs.Dropdown(["Yes", "No", "No internet service"], label="StreamingMovies"),
-        gr.inputs.Dropdown(["Month-to-month", "One year", "Two year"], label="Contract"),
-        gr.inputs.Dropdown(["Yes", "No"], label="PaperlessBilling"),
-        gr.inputs.Dropdown([
-            "Electronic check", "Mailed check", "Bank transfer (automatic)", "Credit card (automatic)"
-        ], label="PaymentMethod"),
-        gr.inputs.Number(label="MonthlyCharges"),
-        gr.inputs.Number(label="TotalCharges")
-    ],
-    outputs=output,  theme="freddyaboulton/dracula_revamped"
-)
 
-iface.launch(share= True )
+# iface = gr.Interface(title= "Customer Churn Prediction For Vodafone PLC",
+#     fn=predict_churn,
+#     inputs=[
+#         gr.inputs.Slider(minimum=0, maximum= 1, step=1, label="SeniorCitizen: Select 1 for Yes and 0 for No"),
+#         gr.inputs.Dropdown(["Yes", "No"], label="Partner: Do You Have a Partner?"),
+#         gr.inputs.Dropdown(["Yes", "No"], label="Dependents: Do You Have a Dependent?"),
+#         gr.inputs.Number(label="tenure: How Long Have You Been with Vodafone in Months?"),
+#         gr.inputs.Dropdown(["DSL", "Fiber optic", "No"], label="InternetService"),
+#         gr.inputs.Dropdown(["Yes", "No", "No internet service"], label="OnlineSecurity"),
+#         gr.inputs.Dropdown(["Yes", "No", "No internet service"], label="OnlineBackup"),
+#         gr.inputs.Dropdown(["Yes", "No", "No internet service"], label="DeviceProtection"),
+#         gr.inputs.Dropdown(["Yes", "No", "No internet service"], label="TechSupport"),
+#         gr.inputs.Dropdown(["Yes", "No", "No internet service"], label="StreamingTV"),
+#         gr.inputs.Dropdown(["Yes", "No", "No internet service"], label="StreamingMovies"),
+#         gr.inputs.Dropdown(["Month-to-month", "One year", "Two year"], label="Contract"),
+#         gr.inputs.Dropdown(["Yes", "No"], label="PaperlessBilling"),
+#         gr.inputs.Dropdown([
+#             "Electronic check", "Mailed check", "Bank transfer (automatic)", "Credit card (automatic)"
+#         ], label="PaymentMethod"),
+#         gr.inputs.Number(label="MonthlyCharges"),
+#         gr.inputs.Number(label="TotalCharges")
+#     ],
+#     outputs=output,  theme="freddyaboulton/dracula_revamped"
+# )
+
+# iface.launch(share= True )
+
